67af7444cfca6ed05362c5c2d937384496157be5
ptrnov <ptr.nov@gmail.com>
2017-10-11 01:40:38 +0700

    update

--- frontend/backend/laporan/controllers/SalesController.php
+++ frontend/backend/laporan/controllers/SalesController.php
@@ -2,12 +2,22 @@
 
 namespace frontend\backend\laporan\controllers;
 
+use Yii;
 use yii\web\Controller;
+use frontend\backend\laporan\models\TransPenjualanDetail;
+use frontend\backend\laporan\models\TransPenjualanDetailSearch;
+
 
 class SalesController extends Controller
 {
     public function actionIndex()
     {
-        return $this->render('index');
+        $searchModel = new TransPenjualanDetailSearch();
+        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+
+        return $this->render('index', [
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+        ]);
     }
 }
