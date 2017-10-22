07aefcde9d7036cfc2577499d1f43ef504c077db
ptrnov <ptr.nov@gmail.com>
2017-10-22 11:27:49 +0700

    update

--- frontend/backend/laporan/controllers/ArusUangController.php
+++ frontend/backend/laporan/controllers/ArusUangController.php
@@ -2,12 +2,31 @@
 
 namespace frontend\backend\laporan\controllers;
 
+use Yii;
 use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+use yii\data\ArrayDataProvider;
+use yii\helpers\ArrayHelper;
+use frontend\backend\laporan\models\ComponenBulan;
+use frontend\backend\laporan\models\TransPenjualanHeaderSummaryMonthly;
+use frontend\backend\laporan\models\TransPenjualanHeaderSummaryMonthlySearch;
 
 class ArusUangController extends Controller
 {
     public function actionIndex()
     {
-        return $this->render('index');
-    }
+		
+		
+		/*==========================
+		* ARUS KAS MASUK & KELUAR
+		*===========================*/
+        $searchModel = new TransPenjualanHeaderSummaryMonthlySearch(['thn'=>'2017']);
+        $dataProvider = $searchModel->searchKasMasukYear(Yii::$app->request->queryParams);
+
+        return $this->render('index', [
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+        ]);
+    }	
 }
--- frontend/backend/laporan/views/arus-uang/index.php
+++ frontend/backend/laporan/views/arus-uang/index.php
@@ -1,2 +1,128 @@
-<?php 
-echo "Arus Uang";
+<?php
+
+use yii\helpers\Html;
+use kartik\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel frontend\backend\laporan\models\TransPenjualanHeaderSummaryMonthlySearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Trans Penjualan Header Summary Monthlies';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<?php
+$this->registerCss("
+	#arus-masuk-monthofyear .kv-panel {
+		//min-height: 340px;
+		height: 300px;
+	}
+	#arus-masuk-monthofyear .kv-grid-container{
+		height:500px
+	}
+");
+	
+	$arusMasuk= GridView::widget([
+		'id'=>'arus-masuk-monthofyear',
+		'dataProvider' => $dataProvider,
+	   // 'filterModel' => $searchModel,
+		'columns' => [
+			[	
+				'attribute'=>'BULAN_NM',
+				'noWrap'=>false,	
+				'headerOptions'=>[		
+						'style'=>[									
+						'text-align'=>'center',
+						'width'=>'100px',
+						'font-family'=>'tahoma, arial, sans-serif',
+						'font-size'=>'8pt'
+					]
+				],  
+				'contentOptions'=>[
+					'style'=>[
+						'text-align'=>'right	',
+						'font-family'=>'tahoma, arial, sans-serif',
+						'font-size'=>'8pt',						
+					]
+				],
+			],
+			[
+				'attribute'=>'TOTAL_SALES',
+				'label'=>'Total Penjualan',				
+				'noWrap'=>false,	
+				'headerOptions'=>[		
+						'style'=>[									
+						'text-align'=>'center',
+						'width'=>'100px',
+						'font-family'=>'tahoma, arial, sans-serif',
+						'font-size'=>'8pt'
+					]
+				],  
+				'contentOptions'=>[
+					'style'=>[
+						'text-align'=>'right',
+						'font-family'=>'tahoma, arial, sans-serif',
+						'font-size'=>'8pt',						
+					]
+				],
+				'format'=>['decimal', 2],
+				'pageSummaryFunc'=>GridView::F_SUM,
+				'pageSummary'=>true,
+				'pageSummaryOptions' => [
+					'style'=>[
+						'text-align'=>'right',		
+						//'width'=>'12px',
+						'font-family'=>'tahoma',
+						'font-size'=>'8pt',	
+						'text-decoration'=>'underline',
+						'font-weight'=>'bold',
+						'border-left-color'=>'transparant',		
+						'border-left'=>'0px',
+						//'background-color'=>'rgba(76, 22, 11, 0.36)',
+						//'color'=>'white',
+					]
+				],	
+			],
+		    [
+				'class' => 'kartik\grid\ActionColumn',
+				'template' => '{view}',
+				'header'=>'Rincian',
+							
+			]
+		],
+		'toolbar' =>false,
+		'panel'=>[
+			'heading'=>false,
+			//'type'=>'info',
+			'after'=>false,
+			'before'=>'ARUS KAS MASUK',
+			'footer'=>false,
+			
+		],
+		'pjax'=>true,
+		'pjaxSettings'=>[
+			'options'=>[
+				'enablePushState'=>true,
+				'id'=>'arus-masuk-monthofyear',
+			],
+		],
+		'hover'=>true, //cursor select
+		'responsive'=>true,
+		'responsiveWrap'=>false,
+		'bordered'=>false,
+		'striped'=>false,
+		'showPageSummary' => true,
+	]); 
+?>
+<div class="container-fluid" style="font-family: verdana, arial, sans-serif ;font-size: 8pt">
+	<div class="col-xs-12 col-sm-12 col-lg-12" style="font-family: tahoma ;font-size: 9pt;">
+		<div class="row">	
+			<div class="col-xs-12 col-sm-6 col-lg-6" style="font-family: tahoma ;font-size: 9pt;">
+				<?=$arusMasuk?>				
+			</div>
+			<div class="col-xs-12 col-sm-6 col-lg-6" style="font-family: tahoma ;font-size: 9pt;">
+				<?=$arusMasuk?>				
+			</div>
+		</div>
+	</div>
+</div>
+
