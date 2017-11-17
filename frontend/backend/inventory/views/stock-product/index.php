e5208ee383ad6d5819cc4c3bcdc1270ced340a3c
ptrnov <ptr.nov@gmail.com>
2017-11-17 23:48:22 +0700

    update

--- frontend/backend/inventory/views/stock-product/index.php
+++ frontend/backend/inventory/views/stock-product/index.php
@@ -16,7 +16,7 @@ use yii\web\View;
 
 	$this->registerCss("
 		#prodak-inv .kv-grid-container{
-			height:560px
+			height:500px
 		}
 	");
 	$pageNm='<span class="fa-stack fa-xs text-left">
@@ -54,13 +54,14 @@ use yii\web\View;
 			],					
 	];
 	
-	$aryFieldColomn[]=['ID' =>0, 'ATTR' =>['FIELD'=>'STORE_NM','SIZE' => '12px','label'=>'TOKO','align'=>'left']];
-	$aryFieldColomn[]=['ID' =>1, 'ATTR' =>['FIELD'=>'PRODUCT_NM','SIZE' => '12px','label'=>'PRODUK','align'=>'left']];
-	$headerContent1[]=['content'=>'DATA PRODUK','options'=>['colspan'=>3,'class'=>'text-center','style'=>'background-color:'.$colorHeader1.';font-family: tahoma ;font-size: 6pt;']];
-	$aryFieldColomn[]=['ID' =>2, 'ATTR' =>['FIELD'=>'STOCK_AWAL','SIZE' => '12px','label'=>'AWAL','align'=>'left']];
-	$aryFieldColomn[]=['ID' =>3, 'ATTR' =>['FIELD'=>'TTL_STOCK_BARU','SIZE' => '12px','label'=>'BARU','align'=>'left']];
-	$aryFieldColomn[]=['ID' =>4, 'ATTR' =>['FIELD'=>'TTL_STOCK_TERJUAL','SIZE' => '12px','label'=>'TERJUAL','align'=>'left']];
-	$aryFieldColomn[]=['ID' =>5, 'ATTR' =>['FIELD'=>'TTL_STOCK_SISA','SIZE' => '12px','label'=>'SISA','align'=>'left']];
+	$aryFieldColomn[]=['ID' =>0, 'ATTR' =>['FIELD'=>'STORE_ID','SIZE' => '10px','label'=>'TOKO','align'=>'left','group'=>true,'pageSummary'=>false]];
+	// $aryFieldColomn[]=['ID' =>1, 'ATTR' =>['FIELD'=>'STORE_NM','SIZE' => '12px','label'=>'TOKO','align'=>'left','group'=>false,'pageSummary'=>false]];
+	$aryFieldColomn[]=['ID' =>1, 'ATTR' =>['FIELD'=>'PRODUCT_NM','SIZE' => '7px','label'=>'PRODUK','align'=>'left','group'=>false,'pageSummary'=>false]];
+	$headerContent1[]=['content'=>'DATA PRODUK','options'=>['colspan'=>2,'class'=>'text-center','style'=>'background-color:'.$colorHeader1.';font-family: tahoma ;font-size: 6pt;']];
+	$aryFieldColomn[]=['ID' =>2, 'ATTR' =>['FIELD'=>'STOCK_AWAL','SIZE' => '7px','label'=>'AWAL','align'=>'right','group'=>false,'pageSummary'=>false]];
+	$aryFieldColomn[]=['ID' =>3, 'ATTR' =>['FIELD'=>'TTL_STOCK_BARU','SIZE' => '7px','label'=>'BARU','align'=>'right','group'=>false,'pageSummary'=>false]];
+	$aryFieldColomn[]=['ID' =>4, 'ATTR' =>['FIELD'=>'TTL_STOCK_TERJUAL','SIZE' => '7px','label'=>'TERJUAL','align'=>'right','group'=>false,'pageSummary'=>false]];
+	$aryFieldColomn[]=['ID' =>5, 'ATTR' =>['FIELD'=>'TTL_STOCK_SISA','SIZE' => '7px','label'=>'SISA','align'=>'right','group'=>false,'pageSummary'=>false]];
 	$headerContent1[]=['content'=>'TOTAL QTY STOCK','options'=>['colspan'=>4,'class'=>'text-center','style'=>'background-color:'.$colorHeader1.';font-family: tahoma ;font-size: 6pt;']];
 				
 	$inc=6;
@@ -75,35 +76,35 @@ use yii\web\View;
 			if($splt[0]=='IN'){
 				$nmField1[]=$rows;		//FULL FIELD NAME
 				$nmLabel[]=$splt[0];	//SPLIT LABEL NAME
-				$aryFieldColomn[]=['ID' =>$inc, 'ATTR' =>['FIELD'=>$rows,'SIZE' => '6px','label'=>'Masuk','align'=>'center','BCOLOR'=>$colorHeader]];
+				$aryFieldColomn[]=['ID' =>$inc, 'ATTR' =>['FIELD'=>$rows,'SIZE'=>'7px','label'=>'Masuk','align'=>'right','group'=>false,'pageSummary'=>true,'BCOLOR'=>$colorHeader]];
 				$inc=$inc+1;
 			}
 			if($splt[0]=='OUT'){
 				$nmField1[]=$rows;		//FULL FIELD NAME
 				$nmLabel[]=$splt[0];	//SPLIT LABEL NAME
-				$aryFieldColomn[]=['ID' =>$inc, 'ATTR' =>['FIELD'=>$rows,'SIZE' => '6px','label'=>'Keluar','align'=>'center','BCOLOR'=>$colorHeader]];
+				$aryFieldColomn[]=['ID' =>$inc, 'ATTR' =>['FIELD'=>$rows,'SIZE'=>'7px','label'=>'Keluar','align'=>'right','group'=>false,'pageSummary'=>false,'BCOLOR'=>$colorHeader]];
 				$inc=$inc+1;
 			}
 			if($splt[0]=='SISA'){
 				$nmField1[]=$rows;		//FULL FIELD NAME
 				$nmLabel[]=$splt[0];	//SPLIT LABEL NAME
-				$aryFieldColomn[]=['ID' =>$inc, 'ATTR' =>['FIELD'=>$rows,'SIZE' => '6px','label'=>'Sisa','align'=>'center','BCOLOR'=>$colorHeader]];
+				$aryFieldColomn[]=['ID' =>$inc, 'ATTR' =>['FIELD'=>$rows,'SIZE'=>'7px','label'=>'Sisa','align'=>'right','group'=>false,'pageSummary'=>false,'BCOLOR'=>$colorHeader]];
 				$inc=$inc+1;
 				$headerContent1[]=['content'=>date('Y-m-d', strtotime($splt[1])),'options'=>['colspan'=>3,'class'=>'text-center','style'=>'background-color:'.$colorHeader1.';font-family: tahoma ;font-size: 6pt;']];		
 			}
 		};
 	 }else{
 		 for ($i=1;$i<=31;$i++){
-			$aryFieldColomn[]=['ID' =>$incTmp, 'ATTR' =>['FIELD'=>$i,'SIZE' => '6px','label'=>$i,'align'=>'center','BCOLOR'=>$colorHeader]];
+			$aryFieldColomn[]=['ID' =>$incTmp, 'ATTR' =>['FIELD'=>$i,'SIZE' => '7px','label'=>$i,'align'=>'right','group'=>false,'pageSummary'=>false,'BCOLOR'=>$colorHeader]];
 			$incTmp=$incTmp+1;
 		 }
 		 $headerContent1[]=['content'=>'STOK TERJUAL','options'=>['colspan'=>$i,'class'=>'text-center','style'=>'background-color:'.$colorHeader1.';font-family: tahoma ;font-size: 6pt;']];			
 	 };
 	 
 	 //OPNAME
-	 $aryFieldColomn[]=['ID' =>$inc, 'ATTR' =>['FIELD'=>'STOCK_AKHIR','SIZE' => '6px','label'=>'Closing','align'=>'center','BCOLOR'=>$colorHeader]];
+	 $aryFieldColomn[]=['ID' =>$inc, 'ATTR' =>['FIELD'=>'STOCK_AKHIR','SIZE' => '7px','label'=>'Closing','align'=>'right','group'=>false,'pageSummary'=>false,'BCOLOR'=>$colorHeader]];
 	 $inc=$inc+1;
-	 $aryFieldColomn[]=['ID' =>$inc, 'ATTR' =>['FIELD'=>'STOCK_AKHIR_ACTUAL','SIZE' => '6px','label'=>'Actual','align'=>'center','BCOLOR'=>$colorHeader]];
+	 $aryFieldColomn[]=['ID' =>$inc, 'ATTR' =>['FIELD'=>'STOCK_AKHIR_ACTUAL','SIZE' => '7px','label'=>'Actual','align'=>'right','group'=>false,'pageSummary'=>false,'BCOLOR'=>$colorHeader]];
 	 $headerContent1[]=['content'=>'STOCK OPNAME','options'=>['colspan'=>2,'class'=>'text-center','style'=>'background-color:'.$colorHeader1.';font-family: tahoma ;font-size: 6pt;']];		
 
 	$valFields = ArrayHelper::map($aryFieldColomn, 'ID', 'ATTR');
@@ -118,8 +119,22 @@ use yii\web\View;
 				//'filterInputOptions'=>$filterInputOpt,				
 				'hAlign'=>'right',
 				'vAlign'=>'middle',
-				//'mergeHeader'=>true,
-				'noWrap'=>true,			
+				'hidden'=>false,
+				'noWrap'=>true,	
+				'value'=>function($data)use($key,$value){
+					$x=$value[$key]['FIELD'];	
+					$splt=explode('_',$x);
+					//if($splt[0]=='SISA'){					
+					if($x=='STORE_ID'){
+						return 'NAMA TOKO :  '.$data['STORE_NM'];
+					}else{						
+						if($data[$x]){
+							return $data[$x];
+						}else{
+							return 0;
+						}						
+					}		
+				},							
 				'headerOptions'=>[		
 					'style'=>[									
 						'text-align'=>'center',
@@ -132,12 +147,45 @@ use yii\web\View;
 				],
 				'contentOptions'=>[
 					'style'=>[
-						'text-align'=>'left',
+						'text-align'=>$value[$key]['align'],
 						'font-family'=>'tahoma, arial, sans-serif',
-						'font-size'=>'7pt',
+						'font-size'=>$value[$key]['SIZE'],
 						'font-weight'=>'bold',
 					]
-				],
+				],				
+				'group'=>$value[$key]['group'],
+				'groupedRow'=>true,
+				'groupFooter'=>function($model, $key, $index, $widget){ 
+					return [
+						'mergeColumns'=>[[2,2]], 
+						'content'=>[             // content to show in each summary cell
+							2=>'TOTAL KESELURUHAN',
+							3=>GridView::F_SUM,4=>GridView::F_SUM,5=>GridView::F_SUM,6=>GridView::F_SUM,7=>GridView::F_SUM,8=>GridView::F_SUM,9=>GridView::F_SUM,10=>GridView::F_SUM,
+							11=>GridView::F_SUM,12=>GridView::F_SUM,13=>GridView::F_SUM,14=>GridView::F_SUM,15=>GridView::F_SUM,16=>GridView::F_SUM,17=>GridView::F_SUM,18=>GridView::F_SUM,19=>GridView::F_SUM,20=>GridView::F_SUM,							
+							21=>GridView::F_SUM,22=>GridView::F_SUM,23=>GridView::F_SUM,24=>GridView::F_SUM,25=>GridView::F_SUM,26=>GridView::F_SUM,27=>GridView::F_SUM,28=>GridView::F_SUM,29=>GridView::F_SUM,30=>GridView::F_SUM,
+							31=>GridView::F_SUM,32=>GridView::F_SUM,33=>GridView::F_SUM,34=>GridView::F_SUM,35=>GridView::F_SUM,36=>GridView::F_SUM,37=>GridView::F_SUM,38=>GridView::F_SUM,39=>GridView::F_SUM,40=>GridView::F_SUM,												
+							41=>GridView::F_SUM,42=>GridView::F_SUM,43=>GridView::F_SUM,44=>GridView::F_SUM,45=>GridView::F_SUM,46=>GridView::F_SUM,47=>GridView::F_SUM,48=>GridView::F_SUM,49=>GridView::F_SUM,50=>GridView::F_SUM,					
+							51=>GridView::F_SUM,52=>GridView::F_SUM,53=>GridView::F_SUM,54=>GridView::F_SUM,55=>GridView::F_SUM,56=>GridView::F_SUM,57=>GridView::F_SUM,58=>GridView::F_SUM,59=>GridView::F_SUM,60=>GridView::F_SUM,					
+							61=>GridView::F_SUM,62=>GridView::F_SUM,63=>GridView::F_SUM,64=>GridView::F_SUM,65=>GridView::F_SUM,66=>GridView::F_SUM,67=>GridView::F_SUM,68=>GridView::F_SUM,69=>GridView::F_SUM,70=>GridView::F_SUM,					
+							71=>GridView::F_SUM,72=>GridView::F_SUM,73=>GridView::F_SUM,74=>GridView::F_SUM,75=>GridView::F_SUM,76=>GridView::F_SUM,77=>GridView::F_SUM,78=>GridView::F_SUM,79=>GridView::F_SUM,80=>GridView::F_SUM,					
+							81=>GridView::F_SUM,82=>GridView::F_SUM,83=>GridView::F_SUM,84=>GridView::F_SUM,85=>GridView::F_SUM,86=>GridView::F_SUM,87=>GridView::F_SUM,88=>GridView::F_SUM,89=>GridView::F_SUM,90=>GridView::F_SUM,					
+							91=>GridView::F_SUM,92=>GridView::F_SUM,93=>GridView::F_SUM,94=>GridView::F_SUM,95=>GridView::F_SUM,96=>GridView::F_SUM,97=>GridView::F_SUM,98=>GridView::F_SUM,99=>GridView::F_SUM,100=>GridView::F_SUM,																	
+						],
+						// 'contentFormats'=>[      // content html attributes for each summary cell
+							// 6=>['format'=>'number', 'decimals'=>2],
+							// 8=>['format'=>'number', 'decimals'=>2],
+							// 9=>['format'=>'number', 'decimals'=>2],
+						// ],
+						'contentOptions'=>[      // content html attributes for each summary cell
+							2=>['style'=>'text-align:right;font-size:9px;font-weight:bold;color:#243852'],
+							// 6=>['style'=>'font-variant:small-caps;text-align:right;color:white','font-size'=>'8pt'],
+							// 8=>['style'=>'font-variant:small-caps;text-align:right;color:white','font-size'=>'8pt'],
+							// 9=>['style'=>'font-variant:small-caps;text-align:right;color:white','font-size'=>'8pt'],
+						],
+						'options'=>['class'=>'danger','style'=>'id:header-sales-trans-x1,font-weight:bold;font-size:8px;text-align:right;']
+					];
+				},
+				
 			];
 		}else{
 			$attDinamikField[]=[		
@@ -149,22 +197,21 @@ use yii\web\View;
 				//'filterInputOptions'=>$filterInputOpt,				
 				'hAlign'=>'right',
 				'vAlign'=>'middle',
-				//'mergeHeader'=>true,
 				'noWrap'=>true,	
 				'value'=>function($data)use($key,$value){
-					$x=$value[$key]['FIELD'];
-					$splt=explode('_',$x);	
-					// if($splt[0]=='SISA'){
-						// $l+=$l+12;
-						// return $l;
-					// }else{						
+					$x=$value[$key]['FIELD'];	
+					$splt=explode('_',$x);
+					//if($splt[0]=='SISA'){					
+					if($x=='STORE_ID'){
+						return 'NAMA TOKO :  '.$data['STORE_NM'];
+					}else{						
 						if($data[$x]){
 							return $data[$x];
 						}else{
 							return 0;
 						}						
-					// }		
-				},				
+					}		
+				},					
 				'headerOptions'=>[		
 						'style'=>[									
 						'text-align'=>'center',
@@ -178,25 +225,45 @@ use yii\web\View;
 				'format' => 'raw',
 				'contentOptions'=>[
 					'style'=>[
-						'text-align'=>'right',
+						'text-align'=>$value[$key]['align'],
 						'font-family'=>'tahoma, arial, sans-serif',
-						'font-size'=>'7px',
+						'font-size'=>$value[$key]['SIZE'],
 						'font-weight'=>'bold',
 						]
-				],
-				//'pageSummaryFunc'=>GridView::F_SUM,
-				//'pageSummary'=>true,
-				'pageSummaryOptions' => [
-					'style'=>[
-							'text-align'=>'right',		
-							'font-family'=>'tahoma',
-							'font-size'=>'7pt',	
-							'text-decoration'=>'underline',
-							'font-weight'=>'bold',
-							'border-left-color'=>'transparant',		
-							'border-left'=>'0px',									
-					]
-				],	
+				],				
+				'group'=>$value[$key]['group'],
+				'groupedRow'=>true,
+				'groupFooter'=>function($model, $key, $index, $widget){ 
+					return [
+						'mergeColumns'=>[[2,2]], 
+						'content'=>[             // content to show in each summary cell
+							2=>'TOTAL KESELURUHAN',
+							3=>GridView::F_SUM,4=>GridView::F_SUM,5=>GridView::F_SUM,6=>GridView::F_SUM,7=>GridView::F_SUM,8=>GridView::F_SUM,9=>GridView::F_SUM,10=>GridView::F_SUM,
+							11=>GridView::F_SUM,12=>GridView::F_SUM,13=>GridView::F_SUM,14=>GridView::F_SUM,15=>GridView::F_SUM,16=>GridView::F_SUM,17=>GridView::F_SUM,18=>GridView::F_SUM,19=>GridView::F_SUM,20=>GridView::F_SUM,							
+							21=>GridView::F_SUM,22=>GridView::F_SUM,23=>GridView::F_SUM,24=>GridView::F_SUM,25=>GridView::F_SUM,26=>GridView::F_SUM,27=>GridView::F_SUM,28=>GridView::F_SUM,29=>GridView::F_SUM,30=>GridView::F_SUM,
+							31=>GridView::F_SUM,32=>GridView::F_SUM,33=>GridView::F_SUM,34=>GridView::F_SUM,35=>GridView::F_SUM,36=>GridView::F_SUM,37=>GridView::F_SUM,38=>GridView::F_SUM,39=>GridView::F_SUM,40=>GridView::F_SUM,												
+							41=>GridView::F_SUM,42=>GridView::F_SUM,43=>GridView::F_SUM,44=>GridView::F_SUM,45=>GridView::F_SUM,46=>GridView::F_SUM,47=>GridView::F_SUM,48=>GridView::F_SUM,49=>GridView::F_SUM,50=>GridView::F_SUM,					
+							51=>GridView::F_SUM,52=>GridView::F_SUM,53=>GridView::F_SUM,54=>GridView::F_SUM,55=>GridView::F_SUM,56=>GridView::F_SUM,57=>GridView::F_SUM,58=>GridView::F_SUM,59=>GridView::F_SUM,60=>GridView::F_SUM,					
+							61=>GridView::F_SUM,62=>GridView::F_SUM,63=>GridView::F_SUM,64=>GridView::F_SUM,65=>GridView::F_SUM,66=>GridView::F_SUM,67=>GridView::F_SUM,68=>GridView::F_SUM,69=>GridView::F_SUM,70=>GridView::F_SUM,					
+							71=>GridView::F_SUM,72=>GridView::F_SUM,73=>GridView::F_SUM,74=>GridView::F_SUM,75=>GridView::F_SUM,76=>GridView::F_SUM,77=>GridView::F_SUM,78=>GridView::F_SUM,79=>GridView::F_SUM,80=>GridView::F_SUM,					
+							81=>GridView::F_SUM,82=>GridView::F_SUM,83=>GridView::F_SUM,84=>GridView::F_SUM,85=>GridView::F_SUM,86=>GridView::F_SUM,87=>GridView::F_SUM,88=>GridView::F_SUM,89=>GridView::F_SUM,90=>GridView::F_SUM,					
+							91=>GridView::F_SUM,92=>GridView::F_SUM,93=>GridView::F_SUM,94=>GridView::F_SUM,95=>GridView::F_SUM,96=>GridView::F_SUM,97=>GridView::F_SUM,98=>GridView::F_SUM,99=>GridView::F_SUM,100=>GridView::F_SUM,																	
+						],
+						'contentFormats'=>[      // content html attributes for each summary cell
+							// 6=>['format'=>'number', 'decimals'=>2],
+							// 8=>['format'=>'number', 'decimals'=>2],
+							// 9=>['format'=>'number', 'decimals'=>2],
+						] 	,
+						'contentOptions'=>[      // content html attributes for each summary cell
+							2=>['style'=>'text-align:right;font-size:9px;font-weight:bold;color:#243852'],
+							// 6=>['style'=>'font-variant:small-caps;text-align:right;color:white','font-size'=>'8pt'],
+							// 8=>['style'=>'font-variant:small-caps;text-align:right;color:white','font-size'=>'8pt'],
+							// 9=>['style'=>'font-variant:small-caps;text-align:right;color:white','font-size'=>'8pt'],
+						],
+						'options'=>['class'=>'danger','style'=>'id:header-sales-trans-x1,font-weight:bold;font-size:8px;text-align:right;']
+					];
+				},
+							
 			];	
 		}
 	};
@@ -220,8 +287,7 @@ use yii\web\View;
 			'type'=>'info',
 			'heading'=>$pageNm,
 			'before'=>false,
-			'after'=>false,
-			'footer'=>false,
+			'after'=>false			
 		],
 		'pjax'=>true,
 		'pjaxSettings'=>[
@@ -245,8 +311,8 @@ use yii\web\View;
 		'summary'=>false,
 		//'floatHeader'=>false,
 		// 'floatHeaderOptions'=>['scrollingTop'=>'200'] 
-		// 'floatOverflowContainer'=>true,
-		//'floatHeader'=>true,
+		'floatOverflowContainer'=>true,
+		'floatHeader'=>true,
 	]);
 ?>
 <div class="container-fluid" style="font-family: verdana, arial, sans-serif ;font-size: 8pt">
