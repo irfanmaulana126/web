0a3c3ab0c2c8e9f22623bf9f73562daa9915c644
Piter Novian <piter@lipat.co.id>
2017-12-04 11:29:06 +0700

    update

--- frontend/config/main-local.php
+++ frontend/config/main-local.php
@@ -4,7 +4,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'dWut4SrmYAaXg0NfqpPwnJa23RMIUG7j_first',
+            'cookieValidationKey' => 'dWut4SrmYAaXg0NfqpPwnJa23RMIUG7j_localhost',
         ],
     ],
 ];
