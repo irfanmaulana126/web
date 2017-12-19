7582a522899e9d002fcd0b235cc2fa85b8f44647
penampungan <syaka.nov@gmail.com>
2017-12-19 11:49:54 +0700

    update

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -6,7 +6,7 @@ use yii\widgets\Breadcrumbs;
 use common\widgets\Alert;
 use frontend\assets\AppAsset;
 AppAsset::register($this);
-
+dmstr\web\AdminLteAsset::register($this);
 	
 ?>
 <?php $this->beginPage() ?>
@@ -15,6 +15,7 @@ AppAsset::register($this);
 		<head>
 			<meta charset="<?= Yii::$app->charset ?>">
 			<meta name="viewport" content="width=device-width, initial-scale=1">
+			 <link rel="shortcut icon" href="<?php echo Yii::$app->request->baseUrl; ?>/favicon.ico" type="image/x-icon" />
 			<?= Html::csrfMetaTags() ?>
 			<title><?= Html::encode($this->title) ?></title>
 			<?php $this->head() ?>
