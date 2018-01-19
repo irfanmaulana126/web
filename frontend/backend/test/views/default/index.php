0534979dae8ebf32e3368d8dd181328106ad81f3
penampungan <syaka.nov@gmail.com>
2018-01-19 18:09:07 +0700

    update

--- frontend/backend/test/views/default/index.php
+++ frontend/backend/test/views/default/index.php
@@ -1,2 +1,17 @@
 <?php 
 echo "default";
+use kartik\field\FieldRange;
+use kartik\widgets\ActiveForm;
+
+$form = ActiveForm::begin();
+
+echo FieldRange::widget([
+    'form' => $form,
+    'model' => $model,
+    'label' => 'Enter amount range',
+    'attribute1' => 'INDUSTRY_ID',
+    'attribute2' => 'INDUSTRY_ID',
+    'type' => FieldRange::INPUT_SPIN,
+]);
+
+?>
