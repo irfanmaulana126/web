0c89fecf886791a41c91de9e1086ef479e15c792
irfan <irfanmaulana126>
2018-01-26 18:37:23 +0700

    update 26/1/2018

---
+++ frontend/backend/hris/views/setelan-presensi/modal_presensi.php
@@ -0,0 +1,52 @@
+<?php
+use yii\bootstrap\Modal;
+use yii\helpers\Html;
+use yii\helpers\Url;
+use kartik\widgets\ActiveForm;
+use kartik\widgets\FileInput;
+use yii\base\DynamicModel;
+
+$this->registerCss("
+	/**
+	 * CSS - Border radius Sudut.
+	 * piter novian [ptr.nov@gmail.com].
+	 * 'clientOptions' => [
+	 *		'backdrop' => FALSE, //Static=disable, false=enable
+	 *		'keyboard' => TRUE,	// Kyboard 
+	 *	]
+	*/
+	.modal-content { 
+		border-radius: 5px;
+	}
+	
+");
+
+	$modalHeaderColor='#fbfbfb';//' rgba(74, 206, 231, 1)';
+
+	/*
+	 * BUTTON - Search
+	*/
+	Modal::begin([
+		//'id' => 'sync_save',
+		'id' => 'presensi-button-jam-modal',
+		'header' => '
+			<span class="fa-stack fa-xs">																	
+				<i class="fa fa-circle fa-stack-2x " style="color:red"></i>
+				<i class="fa fa-pencil fa-stack-1x" style="color:#fbfbfb"></i>
+			</span><b> Restore Toko </b>
+		',	
+		'size' => 'modal-md',
+		'options' => ['class'=>'slide'],
+		'headerOptions'=>[
+			'style'=> 'border-radius:5px; background-color:'.$modalHeaderColor,
+			//'toggleButton' => ['label' => 'click me'],
+		],
+		//'clientOptions' => ['backdrop' => 'static', 'keyboard' => TRUE]
+		'clientOptions' => [
+			'backdrop' => FALSE, //Static=disable, false=enable
+			'keyboard' => TRUE,	// Kyboard 
+		]
+	]);
+		echo "<div id='presensi-button-jam-content'></div>";
+	Modal::end();
+?>
