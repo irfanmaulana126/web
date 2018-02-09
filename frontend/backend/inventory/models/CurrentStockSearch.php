7a77e53dce718d56a5d1b0d26e67ae5c608836d2
penampungan <syaka.nov@gmail.com>
2018-02-09 13:49:36 +0700

    update

---
+++ frontend/backend/inventory/models/CurrentStockSearch.php
@@ -0,0 +1,147 @@
+<?php
+
+namespace frontend\backend\inventory\models;
+
+use Yii;
+use yii\base\Model;
+use yii\base\DynamicModel;
+use yii\helpers\ArrayHelper;
+use yii\helpers\Json;
+use yii\web\Response;
+use yii\data\ArrayDataProvider;
+use yii\debug\components\search\Filter;
+use yii\debug\components\search\matchers;
+
+class CurrentStockSearch extends DynamicModel
+{
+	public $ACCESS_GROUP;
+	public $STORE_ID;
+	public $STORE_NM;
+	public $NAMA_TOKO;
+	public $TAHUN;
+	public $BULAN;
+	public $PRODUCT_ID;
+	public $PRODUCT_NM;
+	public $TTL_QTY;
+
+	
+	public function rules()
+    {
+        return [
+           [['ACCESS_GROUP','STORE_ID','STORE_NM','NAMA_TOKO','TAHUN', 'BULAN','PRODUCT_ID','PRODUCT_NM','TTL_QTY','thn'], 'safe'],
+		];	
+
+    }	
+		
+	public function searchDayOfMonthStock($params){		
+      	$accessGroup=Yii::$app->getUserOpt->user()['ACCESS_GROUP'];//'170726220936';
+		$TGL=$this->thn!=''?$this->thn:date('Y-m-d');
+		$sql="
+			SET SESSION GROUP_CONCAT_MAX_LEN = 1000000;
+				SET @tglIN=concat(date_format(LAST_DAY('".$TGL."' - interval 0 month),'%Y-%m-'),'01');
+				SET @tglOUT=LAST_DAY('".$TGL."');
+				DROP TEMPORARY TABLE IF EXISTS tmp_".$accessGroup.";
+				CREATE TEMPORARY TABLE tmp_".$accessGroup." as(
+					SELECT a.Date as TGL_RUN
+					FROM (
+						select @tglOUT - INTERVAL (a.a + (10 * b.a) + (100 * c.a)) DAY as Date
+						from (select 0 as a union all select 1 union all select 2 union all select 3 union all select 4 union all select 5 union all select 6 union all select 7 union all select 8 union all select 9) as a
+						cross join (select 0 as a union all select 1 union all select 2 union all select 3 union all select 4 union all select 5 union all select 6 union all select 7 union all select 8 union all select 9) as b
+						cross join (select 0 as a union all select 1 union all select 2 union all select 3 union all select 4 union all select 5 union all select 6 union all select 7 union all select 8 union all select 9) as c
+					) a
+					WHERE a.Date BETWEEN @tglIN AND @tglOUT ORDER BY a.Date ASC
+				);
+				SELECT				
+					GROUP_CONCAT(DISTINCT					
+						CONCAT(
+							\"MAX(CASE WHEN DATE_FORMAT(inv.TGL,'%Y-%m-%d') = '\",
+							DATE_FORMAT(str1.TGL_RUN,'%Y-%m-%d'),
+							\"' THEN inv.STOCK_BARU ELSE 0 END) AS 'IN_\",DATE_FORMAT(str1.TGL_RUN,'%Y-%m-%d'),\"',\"		#Stock Baru	per-hari									
+						),
+						CONCAT(
+							\"MAX(CASE WHEN DATE_FORMAT(inv.TGL,'%Y-%m-%d') = '\",
+							DATE_FORMAT(str1.TGL_RUN,'%Y-%m-%d'),
+							\"' THEN inv.STOCK_TERJUAL ELSE 0 END) AS 'OUT_\",DATE_FORMAT(str1.TGL_RUN,'%Y-%m-%d'),\"',\"	#Stock Terjual per-hari										
+						),					
+						CONCAT(
+							\"MAX(CASE WHEN DATE_FORMAT(inv.TGL,'%Y-%m-%d') = '\",
+							DATE_FORMAT(str1.TGL_RUN,'%Y-%m-%d'),
+							\"' THEN inv.STOCK_SISA ELSE 0 END) AS 'SISA_\",DATE_FORMAT(str1.TGL_RUN,'%Y-%m-%d'),\"'\"		#Stock Sisa	per-hari									
+						)					
+					) into @fildText
+				FROM	
+				tmp_".$accessGroup." str1;						
+		";		
+		$qrySqlField=Yii::$app->production_api->createCommand($sql)->execute();
+		$qrySqlField= Yii::$app->production_api->createCommand("	
+			select @fildText;
+		")->queryAll(); 
+		$dpFieldtext= new ArrayDataProvider([	
+			'allModels'=>$qrySqlField,	
+			'pagination' => [
+				'pageSize' =>10000,
+			],			
+		]);		
+		$rsltField=$dpFieldtext->getModels()[0]['@fildText'];
+				
+		$qrySql= Yii::$app->production_api->createCommand("
+			select 
+				#===========================PR =========================================
+				#=== 1. STOK REFUND - pengembalian stock karena cancel Transaksi     ===
+				#=== 2. STOCK OPNAME (BALANCE OPNAME, sisa stock closing dan actual) ===
+				#===========================PR =========================================
+				inv.STORE_NM,inv.STORE_ID,inv.PRODUCT_NM,							
+				".$rsltField.",							
+				inv.STOCK_AWAL,																					#Stok Bulan Lalu, menjadi stock Awal bulan.
+				sum(inv.STOCK_BARU) AS TTL_STOCK_BARU,															#Penambahan Stok di Bulan berjalan.
+				sum(inv.STOCK_TERJUAL) AS TTL_STOCK_TERJUAL,													#Stok terjual di bulan berjalan.
+				sum(STOCK_LAST_MONTH +inv.STOCK_BARU)-SUM(inv.STOCK_TERJUAL) AS TTL_STOCK_SISA,					#Sisa stok dari, penjumlahan sisa stok bulan lalu, penambahan dan penjualan bulan berjalan.			
+				sum(STOCK_LAST_MONTH +inv.STOCK_BARU)-SUM(inv.STOCK_TERJUAL+STOCK_OPNAME) AS STOCK_AKHIR,		#Stock Akhir Closing tanpa opname.
+				sum(inv.STOCK_OPNAME) AS TTL_STOCK_OPNAME,														#total Balance Stok Opname.
+				sum(STOCK_LAST_MONTH +inv.STOCK_BARU)-SUM(inv.STOCK_TERJUAL+STOCK_OPNAME) AS STOCK_AKHIR_ACTUAL	#Actual Stock setelah opname.
+			from
+			(
+				SELECT  
+					a1.LALU AS STOCK_AWAL,				#Sisa stok bulan lalu
+					a1.MASUK AS STOCK_BARU,				#Penambahan stok bulan berjalan.
+					a1.TERJUAL AS STOCK_TERJUAL,		#Stok penjualan bulan berjalan.
+					a1.OPNAME AS STOCK_OPNAME,			#Stok opname bulan berjalan.
+					a1.SISA AS STOCK_SISA,				#Sisa stock daily of month.
+					a1.STOCK_LAST_MONTH,				#Stok Awal bulan, dari stok bulan lalu.
+					a1.TGL,								#Tanggal day of month.
+					a1.PRODUCT_ID,						#
+					a1.STORE_ID,						#
+					a2.PRODUCT_NM,						#
+					a3.STORE_NM							#															
+				FROM ptr_kasir_inv1c a1 
+				left join product a2 on a2.PRODUCT_ID=a1.PRODUCT_ID
+				left join store a3 on a3.STORE_ID=a1.STORE_ID
+				WHERE a1.ACCESS_GROUP='".$accessGroup."' AND a1.TAHUN=YEAR('".$TGL."') AND a1.BULAN=MONTH('".$TGL."')				
+			) inv
+			GROUP BY inv.STORE_ID,inv.PRODUCT_ID				
+		")->queryAll(); 	
+		$dataProvider= new ArrayDataProvider([	
+			'allModels'=>$qrySql,	
+			'pagination' => [
+				'pageSize' =>10000,
+			],			
+		]);					
+				
+		//return $rsltField;
+		if (!($this->load($params) && $this->validate())) {
+ 			return $dataProvider;
+ 		}
+		
+		$filter = new Filter();
+ 		$this->addCondition($filter, 'ACCESS_GROUP', true);	
+		$this->addCondition($filter, 'STORE_ID', true);	
+		$this->addCondition($filter, 'STORE_NM', true);	
+		$this->addCondition($filter, 'TAHUN', true);
+ 		$this->addCondition($filter, 'BULAN', true);	
+ 		$this->addCondition($filter, 'PRODUCT_ID', true);	
+ 		$this->addCondition($filter, 'PRODUCT_NM', true);	 		
+ 		$this->addCondition($filter, 'TTL_QTY', true);	 		
+ 		$dataProvider->allModels = $filter->filter($qrySql);
+        return $dataProvider; 
+	} 
+}
